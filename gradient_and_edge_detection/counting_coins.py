25addff2d0dded203e89f46f66c49326040fc39a
yang.tang <mic-jasontang@outlook.com>
2018-08-06 22:05:37 +0800

    add some code in countint_coins.py

--- gradient_and_edge_detection/counting_coins.py
+++ gradient_and_edge_detection/counting_coins.py
@@ -22,3 +22,59 @@ cv2.imshow("Blurred", blurred)
 
 edged = cv2.Canny(blurred, 30, 150)
 cv2.imshow("Edged", edged)
+
+# Now that we have the outlines of the coins,
+# we can find the contours of the outlines.
+
+# Return Parameter:
+# after_detection: our image after applying contour detection.
+# cnts: the contours themselves. the type is list.
+# hierarchy: hierarchy(层级) of contours.
+
+# Argument:
+# edged.copy(): edged image
+# cv2.RETR_EXTERNAL: contour type,use cv2.RETR_EXTERNAL to retrieve only
+#   the outermost contours.We can also pass in cv2.RETR_LIST to grab all contours.
+#   Other methods include hierarchical contours using
+#   cv2.RETR_COMP and cv2.RETR_TREE, but hierarchical contours are outside the scope of this book.
+# cv2.CHAIN_APPROX_SIMPLE: this argument is how we want to approximate the
+#   contour.We use cv2.CHAIN_APPROX_SIMPLE to compress
+# horizontal, vertical, and diagonal segments into their endpoints only.
+# This saves both computation and memory. If
+# we wanted all the points along the contour, without compression,
+# we can pass in cv2.CHAIN_APPROX_NONE; however,
+# be very sparing when using this function. Retrieving all
+# points along a contour is often unnecessary and is wasteful
+# of resources.
+(after_detection, cnts, hierarchy) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+								 cv2.CHAIN_APPROX_SIMPLE)
+
+print("hierarchy:{}".format(hierarchy))
+cv2.imshow("After detection", after_detection)
+print("I count {} coins in this image, the list is :{}".format(len(cnts), cnts))
+
+# In order not to draw on our original image,
+# we make a copy of the original image
+coins = image.copy()
+# draws the actual contours on our image.
+
+# The first argument to the function is the image
+# we want to draw on.
+
+# The second is our list of contours.
+
+# The Next, we have the contour index. By specifying a negative
+# value of −1, we are indicating that we want to draw all of
+# the contours. However, we would also supply an index i,
+# which would be the i’th contour in cnts. This would allow
+# us to draw only a single contour rather than all of them.
+
+# The fourth argument to the cv2.drawContours function
+# is the color of the line we are going to draw.
+
+# The last argument is the thickness of the line we
+# are drawing. We’ll draw the contour with a thickness of
+# two pixels
+cv2.drawContours(coins, cnts, -1, (0, 255, 0), 2)
+cv2.imshow("Coins", coins)
+cv2.waitKey(0)
