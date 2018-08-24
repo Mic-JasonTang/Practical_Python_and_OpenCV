6e2bddfa3019940130d7bc22bb5412cb49353295
yang.tang <mic-jasontang@outlook.com>
2018-08-24 22:01:21 +0800

    finsh project face_detection

--- face_detection/detect_faces.py
+++ face_detection/detect_faces.py
@@ -8,6 +8,7 @@
 
 import cv2
 import argparse
+from facedetector import FaceDetector
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--face", required=True,
@@ -18,3 +19,14 @@ args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+fd = FaceDetector(args["face"])
+faceRects = fd.detect(gray, scaleFactor = 1.2, minNeighbors = 5,
+					 minSize = (30, 30))
+print("I found {} face(s)".format(len(faceRects)))
+
+for (x, y, w, h) in faceRects:
+	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow("Faces", image)
+cv2.waitKey(0)
