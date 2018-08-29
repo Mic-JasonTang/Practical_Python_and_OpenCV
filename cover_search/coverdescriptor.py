7d1858ee897e476f8e8f1bf63525f90488d61df7
yang.tang <mic-jasontang@outlook.com>
2018-08-29 18:49:42 +0800

    add project cover_search

---
+++ cover_search/coverdescriptor.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @Author:   MR_Radish
+# @Date:     2018-08-29 18:21:04
+# @E-mail:   ty_2016@foxmail.com
+# @FileName: coverdescriptor.py
+# @TODO:
+
+import numpy as np
+import cv2
+
+class CoverDescriptor:
+
+	def __init__(self, useSIFT = False):
+		self.useSIFT = useSIFT
+
+	def describe(self, image):
+
+		if self.useSIFT:
+			descriptor = cv2.xfeatures2d.SIFT_create()
+		else:
+			descriptor = cv2.BRISK_create()
+		# This method both detects keypoints (i.e., “interesting” regions of an image)
+		# and then describes and quantifies the region surrounding each of the keypoints.
+		# Thus, the keypoint detection is the “detect” phase, whereas the
+		# actual description of the region is the “compute” phase.
+		(kps, descs) = descriptor.detectAndCompute(image, None)
+		# the (x, y) coordinates of the keypoint, contained in the pt attribute.
+		kps = np.float([kp.pt for kp in kps])
+
+		return (kps, descs)
+
